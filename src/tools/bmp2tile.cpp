0be44ec5713fc7d2b51914c0c98d64d53d84b47d
Francis Godden <francisgodden@hotmail.com>
2024-01-09 23:12:25 +0000

    Update bmp2tile to generate tiles

--- src/tools/bmp2tile.cpp
+++ src/tools/bmp2tile.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <memory>
 #include <vector>
@@ -6,12 +7,13 @@
 class Bitmap {
 public:
 
-	Bitmap()
+	Bitmap(const char* filename)
 		: signature(0), fileSize(0), reservedHeader(0), dataOffset(0), 
 		size(0), width(0), height(0), planes(0), bpp(0), compression(0),
 		imageSize(0), xppm(0), yppm(0), coloursUsed(0), importantColours(0),
 		numColours(0)
 	{
+		load(filename);
 	}
 
 	Bitmap(const Bitmap& obj) = default;
@@ -22,16 +24,25 @@ public:
 	
 	bool operator==(const Bitmap& other) const = delete;
 
+	void print() {
+		printHeaderAndBody(); 
+		printColourTable(); 
+		printPixels(); 
+		printGrid(true);
+		printTiles();
+	}
+
+private:
+
 	void load(const char* filename) {
 		std::ifstream fs(filename, std::ios::binary);
 		if (!fs) {
-			throw "Cannot open file.";
+			throw std::runtime_error("Cannot open file.");
 		}
-		Bitmap bitmap;
 		char header[14];
 		fs.read(header, 14);
 		if (header[0] != 'B' && header[1] != 'M') {
-			throw "Not a valid BMP file.";
+			throw std::runtime_error("Not a valid BMP file.");
 		}
 
 		std::memcpy(&signature, &header[0], 2);
@@ -50,12 +61,12 @@ public:
 			numColours = 16;
 		}
 		else {
-			throw "Not a valid Mega Drive pallet - must be 4 bpp = 16 colours.";
+			throw std::runtime_error("Not a valid Mega Drive pallet - must be 4 bpp = 16 colours.");
 		}
 
 		std::memcpy(&compression, &body[16], 4);
 		if (compression != 0) {
-			throw "Requires uncompressed BMP.";
+			throw std::runtime_error("Requires uncompressed BMP.");
 		}
 		std::memcpy(&imageSize, &body[20], 4);
 		std::memcpy(&xppm, &body[24], 4);
@@ -81,22 +92,34 @@ public:
 
 		std::memcpy(pixels.data(), &body[dataIndex], numPixels);
 		flip();
+		buildTiles();
 	}
-
-	void print() {
-		printHeaderAndBody(); 
-		printColourTable(); 
-		printPixels(); 
-		printGrid(true);
-	}
-
-private:
 
 	void flip() {
 		pixelsFlipped.resize(pixels.size());
 		for (unsigned j = 1; j <= height; ++j) {
 			for (unsigned i = 0; i < width / 2; ++i) {
 				pixelsFlipped[i + (j - 1) * (width / 2)] = pixels[i + (width - j) * (width / 2)];
+			}
+		}
+	}
+
+	void buildTiles() {
+		if ((width % 8 != 0) && (height % 8 != 0)) {
+			throw std::runtime_error("Tiles require width and height to be multiples of 8");
+		}
+
+		for (int y = 0; y < height / 8; ++y) {
+			for (int x = 0; x < width / 8; ++x) {
+				int k = 0;
+				Tile tile{};
+				for (int j = 0; j < 8; ++j) {
+					for (int i = 0; i < 4; ++i) {
+						tile.pixels[k++] = pixelsFlipped[i + x * 4 + (width / 2) * (j + y * 8)];
+					}
+				}
+				tiles.push_back(tile);
+				//printTile(tile);
 			}
 		}
 	}
@@ -118,6 +141,7 @@ private:
 		std::cout << "yppm " << yppm << std::endl;
 		std::cout << "coloursUsed " << coloursUsed << std::endl;
 		std::cout << "importantColours " << importantColours << std::endl;
+		std::cout << std::endl;
 	}
 
 	void printColourTable() {
@@ -129,6 +153,7 @@ private:
 				<< ", red " << (int)table.red
 				<< ", reserved " << (int)table.reserved << std::endl;
 		}
+		std::cout << std::endl;
 	}
 
 	void printPixels(bool flipped = false) {
@@ -137,7 +162,7 @@ private:
 		for (int i = 0; i < data.size(); ++i) {
 			std::cout << std::hex << (int)data[i] << " ";
 		}
-		std::cout << std::endl;
+		std::cout << std::endl << std::endl;
 	}
 
 	void printGrid(bool flipped = false) {
@@ -148,6 +173,28 @@ private:
 				std::cout << (int)data[i + j * (width / 2)] << " ";
 			}
 			std::cout << std::endl;
+		}
+		std::cout << std::endl;
+	}
+
+	struct Tile {
+		unsigned char pixels[4 * 8]; // 4 chars per row (1 nibble per pixel)
+	};
+
+	void printTile(const Tile & tile) {
+		for (int j = 0; j < 8; ++j) {
+			for (int i = 0; i < 4; ++i) {
+				std::cout << std::setw(3) << (int)tile.pixels[i + 4*j] << " ";
+			}
+			std::cout << std::endl;
+		}
+		std::cout << std::endl;
+	}
+	void printTiles() {
+		int i = 0;
+		for (const auto& tile : tiles) {
+			std::cout << "tile " << i++ << ": " << std::endl;
+			printTile(tile);
 		}
 	}
 
@@ -179,10 +226,11 @@ private:
 	
 	std::vector<unsigned char> pixels; // each char is 2 pixels
 	std::vector<unsigned char> pixelsFlipped;
+	std::vector<Tile> tiles;
 };
 
-int main(int argc, int **argv) {
-	Bitmap bitmap;
-	bitmap.load("img/image.bmp");
+int main(int argc, char **argv) {
+	std::string filename = (argc < 2) ? "img/image.bmp" : argv[1];
+	Bitmap bitmap{ filename.c_str() };
 	bitmap.print();
 }
